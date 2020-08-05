15213e71603ec0dbe20e630b69368ec52c71bf6b
Carling <carlingkirk@gmail.com>
2020-08-05 07:58:08 -0400

    KISS - Remove command line arguments

--- Program.cs
+++ Program.cs
@@ -12,44 +12,42 @@ namespace PDFJoiner
     {
         static void Main(string[] args)
         {
-            var filepath = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "/Documents/Crosswords");
-            if (args.Any())
-            {
-                filepath = new DirectoryInfo(args[0]);
-            }
+            var filepath = new DirectoryInfo(Directory.GetCurrentDirectory());
 
             var pdfFiles = filepath.EnumerateFiles("*.pdf").OrderBy(f => f.CreationTimeUtc).ToList();
             if (pdfFiles.Count < 2)
             {
-                throw new InvalidOperationException($"Only found {pdfFiles.Count} files!");
+                Console.WriteLine($"Found {pdfFiles.Count} files... nothing to do.");
             }
-
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var pdfResult = new PdfDocument();
-            var font = new XFont("Consolas", 10, XFontStyle.Bold);
-            var format = new XStringFormat
+            else
             {
-                Alignment = XStringAlignment.Center,
-                LineAlignment = XLineAlignment.Far
-            };
-
-            foreach (var file in pdfFiles)
-            {
-                using (var pdfFile = PdfReader.Open(file.FullName, PdfDocumentOpenMode.Import))
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+                var pdfResult = new PdfDocument();
+                var font = new XFont("Consolas", 10, XFontStyle.Bold);
+                var format = new XStringFormat
                 {
-                    pdfResult.Pages.Add(pdfFile.Pages[0]);
+                    Alignment = XStringAlignment.Center,
+                    LineAlignment = XLineAlignment.Far
+                };
 
-                    var page = pdfResult.Pages[pdfResult.Pages.Count - 1];
-                    // Write document file name
-                    var gfx = XGraphics.FromPdfPage(page);
-                    var box = page.MediaBox.ToXRect();
-                    box.Inflate(0, -10);
-                    gfx.DrawString($"{file.Name}", font, XBrushes.Black, box, format);
+                foreach (var file in pdfFiles)
+                {
+                    using (var pdfFile = PdfReader.Open(file.FullName, PdfDocumentOpenMode.Import))
+                    {
+                        pdfResult.Pages.Add(pdfFile.Pages[0]);
+
+                        var page = pdfResult.Pages[^1];
+                        // Write document file name
+                        var gfx = XGraphics.FromPdfPage(page);
+                        var box = page.MediaBox.ToXRect();
+                        box.Inflate(0, -10);
+                        gfx.DrawString($"{file.Name}", font, XBrushes.Black, box, format);
+                    }
                 }
-            }
 
-            var filename = pdfFiles.First().Name.Replace(".pdf", "") + "-" + pdfFiles.Last().Name;
-            pdfResult.Save(filepath + "/" + filename);
+                var filename = pdfFiles.First().Name.Replace(".pdf", "") + "-" + pdfFiles.Last().Name;
+                pdfResult.Save(filepath + "/" + filename);
+            }
         }
     }
 }
